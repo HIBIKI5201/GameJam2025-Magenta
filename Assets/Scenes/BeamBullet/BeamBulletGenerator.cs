bbe6d23df8acf6178b9a8714eb4c0df362bc7365
阿久津匠輝 <shoki.akutsu.vantan@gmail.com>
2025-08-04 17:48:28 +0900

    [add] レーザー弾の作成&Generatorの作成

---
+++ Assets/Scenes/BeamBullet/BeamBulletGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+
+public class BeamBulletGenerator : IBulletGenerator
+{
+	Transform _playerTransform;
+	[SerializeField] GameObject _bullet;
+	
+	public BeamBulletGenerator()
+	{
+		
+	}
+
+	public void Update(float deltaTime)
+	{
+		//自機の少しずらしたところに生成
+		Object.Instantiate(_bullet, _playerTransform);
+	}
+}
