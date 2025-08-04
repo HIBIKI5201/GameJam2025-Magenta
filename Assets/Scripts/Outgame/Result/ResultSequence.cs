5abd0ed218e9c1ae79cad8deef68d04d9924639f
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-05 00:08:51 +0900

    [add]リザルトシーン

---
+++ Assets/Scripts/Outgame/Result/ResultSequence.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultSequence : MonoBehaviour
+{
+    [SerializeField]
+    private string _titleSceneName = "Title"; 
+
+    private async void Start()
+    {
+        
+
+        //一旦タイトルに戻る
+        await Awaitable.WaitForSecondsAsync(1f);
+
+        SceneManager.LoadScene(_titleSceneName);
+    }
+}
