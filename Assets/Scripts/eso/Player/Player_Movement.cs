65bcd6e63b54923bc0ea96359c0f7b903752078e
ESO-gardo-kia <114640824+ESO-gardo-kia@users.noreply.github.com>
2025-08-04 17:51:52 +0900

    プレイヤー基本システム tag及びlayer追加

---
+++ Assets/Scripts/eso/Player/Player_Movement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player_Movement : MonoBehaviour
+{
+    private Func<Vector2> Move_Input;
+    [SerializeField] private float _move_Speed;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Vector3 _movement_Area;
+    [SerializeField] private Vector3 _movement_Pos;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 vec = new Vector3(Move_Input.Invoke().x, Move_Input.Invoke().y, 0) * Time.deltaTime;
+
+        transform.Translate(vec * _move_Speed,Space.Self);
+
+        transform.localPosition = new Vector2(
+            Mathf.Clamp(transform.localPosition.x, -_movement_Area.x, _movement_Area.x),
+            Mathf.Clamp(transform.localPosition.y, -_movement_Area.y, _movement_Area.y)
+            );
+    }
+
+    public void SetAction(Func<Vector2> func)
+    {
+        Move_Input = func;
+    }
+
+    public void SetMovementArea(Vector3 pos, Vector3 area)
+    {
+        _movement_Area = area;
+        _movement_Pos = pos;
+    }
+}
---
+++ Assets/Scripts/eso/Player/TestBulletManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TestBulletManager : MonoBehaviour
+{
+    [SerializeField] GameObject Obj;
+    public float count;
+    public float shotmax;
+
+    public void Shot()
+    {
+        if (count >= shotmax)
+        {
+            count = 0;
+            var obj = Instantiate(Obj, transform.localPosition, transform.localRotation);
+            obj.GetComponent<Rigidbody>().linearVelocity = Vector3.right * 20;
+            obj.GetComponent<TestBullet>().SetShotSideTag(tag);
+            if (tag == "Player1")
+            {
+                obj.tag = "Bullet1";
+            }
+            if (tag == "Player2")
+            {
+                obj.tag = "Bullet2";
+            }
+
+        }
+        else
+        {
+            count += Time.deltaTime;
+        }
+    }
+}
