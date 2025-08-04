861ee360f3c5c1e8bc231264e455c4f18b204bd0
shimamura <shimakou779@gmail.com>
2025-08-04 11:59:08 +0900

    [add]タイトルマネージャーのスクリプト追加

---
+++ Assets/Scripts/Shimamura/TitelManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitelManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TitleSenen()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+    public void OptionSenen()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+    public void StratGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // ビルド後にアプリケーションを終了
+        Application.Quit();
+#endif
+    }
+}
