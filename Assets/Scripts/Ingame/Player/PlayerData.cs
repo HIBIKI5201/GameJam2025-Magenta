0dd40f867d5e73b266a6efbe7b30559a6a98e1cf
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-05 00:35:51 +0900

    no message

--- Assets/Scripts/Ingame/Player/PlayerData.cs
+++ Assets/Scripts/Ingame/Player/PlayerData.cs
@@ -5,9 +5,12 @@ public class PlayerData : ScriptableObject
 {
     public float MaxHealth => _maxHealth;
     public float MoveSpeed => _moveSpeed;
+    public float InvincibilityTime => _invincibilityTime;
 
     [SerializeField]
     private float _moveSpeed = 5f;
     [SerializeField]
     private float _maxHealth = 100f;
+    [SerializeField]
+    private float _invincibilityTime = 0.5f;
 }
