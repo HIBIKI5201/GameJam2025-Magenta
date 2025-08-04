9116131a065555b84a40f2a1051edf6c6c8df0df
shimamura <shimakou779@gmail.com>
2025-08-04 16:54:43 +0900

    [add]オーディオの変更するためのスクリプト作成

---
+++ Assets/Scripts/Shimamura/AudioOption.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+[RequireComponent(typeof(PlayerInput))]
+
+public class AudioOption : MonoBehaviour
+{
+
+    PlayerInput _playerInput;
+
+    [SerializeField] Slider[] sliders;
+    [SerializeField] Outline[] outlines;
+
+    int selectedIndex = 0;
+    float lastInputTime = 0f;
+    float inputDelay = 0.2f; // 入力感度調整（連打防止）
+
+
+    public AudioMixer AudioMixer;
+    public Slider BGMSlider;
+    public Slider SESlider;
+    public Slider MasterSlider;
+    public GameObject firstSelect;
+
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        EventSystem.current.SetSelectedGameObject(firstSelect);
+
+        AudioMixer.GetFloat("BGMVolume", out float bgmVolume);//スライダー設定
+        BGMSlider.value = bgmVolume;
+        AudioMixer.GetFloat("SEVolume", out float seVolume);
+        SESlider.value = seVolume;
+        AudioMixer.GetFloat("MasterVolume", out float mastervolume);
+        MasterSlider.value = mastervolume;
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.actions["Move1"].started += OnAndioOption;
+
+        _playerInput.actions["Move1"].canceled += OnAndioOption;
+
+    }
+    private void OnDisable()
+    {
+        _playerInput.actions["Move1"].started -= OnAndioOption;
+            
+        _playerInput.actions["Move1"].canceled -= OnAndioOption;
+    }
+
+    private void OnAndioOption(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+
+        if (Time.time - lastInputTime < inputDelay)
+            return;
+
+        // 上下キーで選択切替
+        if (input.y > 0.5f)
+        {
+            selectedIndex = (selectedIndex - 1 + sliders.Length) % sliders.Length;
+            UpdateOutLine();
+            lastInputTime = Time.time;
+        }
+        else if (input.y < -0.5f)
+        {
+            selectedIndex = (selectedIndex + 1) % sliders.Length;
+            UpdateOutLine();
+            lastInputTime = Time.time;
+        }
+    }
+
+    private void Update()
+    {
+        if (_playerInput == null) return;
+
+        Vector2 input = _playerInput.actions["Move1"].ReadValue<Vector2>();
+
+        // 左右キーでスライダー値を調整
+        if (input.x > 0.5f)
+        {
+            sliders[selectedIndex].value += Time.deltaTime;
+        }
+        else if (input.x < -0.5f)
+        {
+            sliders[selectedIndex].value -= Time.deltaTime;
+        }
+    }
+
+    private void UpdateOutLine()        
+    {
+        for (int i = 0; i < outlines.Length; i++)
+        {
+            outlines[i].enabled = (i == selectedIndex);
+        }
+    }
+
+    public void SetBGM(float volume)
+    {
+        AudioMixer.SetFloat("BGMVolume", volume);
+    }
+
+    public void SetSE(float volume)
+    {
+        AudioMixer.SetFloat("SEVolume", volume);
+    }
+    public void SetMaster(float volume)
+    {
+        AudioMixer.SetFloat("MasterVolume", volume);
+    }
+}
--- Assets/Scripts/Shimamura/TitelManager.cs
+++ Assets/Scripts/Shimamura/TitelManager.cs
@@ -1,31 +1,68 @@
-﻿using System.Linq;
-using SymphonyFrameWork.Utility;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
-
-
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(PlayerInput))]
 public class TitelManager : MonoBehaviour
 {
+
     [SerializeField] string _TitleScene;
     [SerializeField] string _OptionScene;
     [SerializeField] string _GameScene;
 
-    private InputActionMap _ActionMap;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _ActionMap = new InputActionMap();
 
-      //タイムラインでループすようにする　静止画だとさみしいから
-      //SymphonyTween 時間を何秒かかけて動かしたりすることができる
+    PlayerInput _p;
+    private bool _move1Pressed = false;
+    private bool _move2Pressed = false;
+
+    private void Awake()
+    {
+        _p = GetComponent<PlayerInput>();
+    }
+    private void OnEnable()
+    {
+        _p.actions["Move1"].started += OnMove1Started;
+        _p.actions["Move2"].started += OnMove2Started;
+
+        _p.actions["Move1"].canceled += OnMove1Canceled;
+        _p.actions["Move2"].canceled += OnMove2Canceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-      
+        _p.actions["Move1"].started -= OnMove1Started;
+        _p.actions["Move2"].started -= OnMove2Started;
+
+        _p.actions["Move1"].canceled -= OnMove1Canceled;
+        _p.actions["Move2"].canceled -= OnMove2Canceled;
+    }
+     private void OnMove1Started(InputAction.CallbackContext context)   //プレイヤー1がのボタンを押したらtureになる
+    {
+        _move1Pressed = true;
+        CheckBothPressed();
+    }
+
+    private void OnMove2Started(InputAction.CallbackContext context)    //プレイヤー2がのボタンを押したらtureになる
+    {
+        _move2Pressed = true;
+        CheckBothPressed();
+    }
+
+    private void OnMove1Canceled(InputAction.CallbackContext context)
+    {
+        _move1Pressed = false;
+    }
+
+    private void OnMove2Canceled(InputAction.CallbackContext context)
+    {
+        _move2Pressed = false;
+    }
+    private void CheckBothPressed()
+    {
+        if (_move1Pressed && _move2Pressed)     //プレイヤー1とプレイヤー2が同時押しした時に
+        {
+            Debug.Log("両方押されたのでシーンを移動！");
+            SceneManager.LoadScene(_GameScene); // ゲームシーン
+        }
     }
     public void TitleSenen()    //タイトルシーン
     {
@@ -34,10 +71,6 @@ public class TitelManager : MonoBehaviour
     public void OptionSenen()   //オプションシーン
     {
         SceneManager.LoadScene(_OptionScene);
-    }
-    public void StratGame()     //ゲームシーン    WASDのどれかと十字キーのどれかを押してる状態でスタート
-    {
-        SceneManager.LoadScene(_GameScene);
     }
 
 
