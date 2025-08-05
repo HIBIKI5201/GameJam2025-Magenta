8e16e2c8243700286be97f7e152d5ab90ed2f1dc
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-05 17:29:43 +0900

    [update]最終鬼畜

---
+++ Assets/Scripts/Ingame/ShipManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShipManager : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _velocity;
+    [SerializeField]
+    private float _bounds;
+
+    private Vector3 _initializePos;
+
+    private void Start()
+    {
+        _initializePos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position += _velocity * Time.deltaTime;
+
+        if (_bounds < Vector3.Distance(_initializePos, transform.position))
+        {
+            transform.position = _initializePos;
+        }
+    }
+}
