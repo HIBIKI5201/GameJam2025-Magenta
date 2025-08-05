f6cda637c1166697ded2ca9a0f7b497c5f523eae
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-05 14:10:29 +0900

    [hotfix]シーンのロードをAdditionalに変更

---
+++ Assets/Scripts/Utility/SceneLoadUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadUtility
+{
+    public static async void LoadScene(string sceneName)
+    {
+        Scene current = SceneManager.GetActiveScene();
+        await SceneManager.UnloadSceneAsync(current.name);
+        await SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    }
+}
